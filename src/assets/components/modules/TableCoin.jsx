5d3397df4d31536c9b2d0515185d6fe4c26bb8a6
Mina Talebi <mina.talebi726@gmail.com>
2025-11-06 20:56:13 +0330

    style table

--- src/assets/components/modules/TableCoin.jsx
+++ src/assets/components/modules/TableCoin.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import chartUp from "../../../assets/chart-up.svg";
 import chartDown from "../../../assets/chart-down.svg";
 import { RotatingLines } from "react-loader-spinner";
+import styles from "./TableCoin.module.css";
 function TableCoin({ coins, isLoading }) {
   // console.log(coins);
   return (
-    <div>
+    <div className={styles.container}>
       {isLoading ? (
         <RotatingLines strokeColor="#3874ff" strokeWidth={4} />
       ) : (
-        <table>
+        <table className={styles.table}>
           <thead>
             <tr>
               <th>Coin</th>
@@ -66,14 +67,16 @@ const TableRow = ({
   return (
     <tr>
       <td>
-        <div>
+        <div className={styles.symbol}>
           <img src={image} alt="" />
           <span>{symbol.toUpperCase()}</span>
         </div>
       </td>
       <td>{name}</td>
       <td>${current_price.toLocaleString()}</td>
-      <td>{price_change.toFixed(2)}%</td>
+      <td className={price_change > 0 ? styles.success : styles.error}>
+        {price_change.toFixed(2)}%
+      </td>
       <td>{total_volume.toLocaleString()}</td>
       <td>
         <img src={price_change > 0 ? chartUp : chartDown} alt={name} />
