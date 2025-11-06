3f5e41e2703d9f4322d0d501f14b973e9674582f
Mina Talebi <mina.talebi726@gmail.com>
2025-11-06 20:39:29 +0330

    add loading icon to project

--- src/assets/components/modules/TableCoin.jsx
+++ src/assets/components/modules/TableCoin.jsx
@@ -1,47 +1,52 @@
 import React from "react";
 import chartUp from "../../../assets/chart-up.svg";
 import chartDown from "../../../assets/chart-down.svg";
-function TableCoin({ coins }) {
+import { RotatingLines } from "react-loader-spinner";
+function TableCoin({ coins, isLoading }) {
   // console.log(coins);
   return (
     <div>
-      <table>
-        <thead>
-          <tr>
-            <th>Coin</th>
-            <th>Name</th>
-            <th>Price</th>
-            <th>24h</th>
-            <th>Total Volume</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {coins.map((coin) => (
-            <TableRow coin={coin} key={coin.id} />
-            // <tr key={coin.id}>
-            //   <td>
-            //     <div>
-            //       <img src={coin.image} alt="" />
-            //       <span>{coin.symbol.toUpperCase()}</span>
-            //     </div>
-            //   </td>
-            //   <td>{coin.name}</td>
-            //   <td>${coin.current_price.toLocaleString()}</td>
-            //   <td>{coin.price_change_percentage_24h.toFixed(2)}%</td>
-            //   <td>{coin.total_volume.toLocaleString()}</td>
-            //   <td>
-            //     <img
-            //       src={
-            //         coin.price_change_percentage_24h > 0 ? chartUp : chartDown
-            //       }
-            //       alt={coin.name}
-            //     />
-            //   </td>
-            //</tr>
-          ))}
-        </tbody>
-      </table>
+      {isLoading ? (
+        <RotatingLines strokeColor="#3874ff" strokeWidth={4} />
+      ) : (
+        <table>
+          <thead>
+            <tr>
+              <th>Coin</th>
+              <th>Name</th>
+              <th>Price</th>
+              <th>24h</th>
+              <th>Total Volume</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {coins.map((coin) => (
+              <TableRow coin={coin} key={coin.id} />
+              // <tr key={coin.id}>
+              //   <td>
+              //     <div>
+              //       <img src={coin.image} alt="" />
+              //       <span>{coin.symbol.toUpperCase()}</span>
+              //     </div>
+              //   </td>
+              //   <td>{coin.name}</td>
+              //   <td>${coin.current_price.toLocaleString()}</td>
+              //   <td>{coin.price_change_percentage_24h.toFixed(2)}%</td>
+              //   <td>{coin.total_volume.toLocaleString()}</td>
+              //   <td>
+              //     <img
+              //       src={
+              //         coin.price_change_percentage_24h > 0 ? chartUp : chartDown
+              //       }
+              //       alt={coin.name}
+              //     />
+              //   </td>
+              //</tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 }
