eac286f8bad524554a4b5e8c12414e3c6ba646f9
Mina Talebi <mina.talebi726@gmail.com>
2025-11-07 12:58:53 +0330

    add modal

--- src/assets/components/templates/HomePages.jsx
+++ src/assets/components/templates/HomePages.jsx
@@ -3,11 +3,14 @@ import TableCoin from "../modules/TableCoin";
 import { getCoinList } from "../../services/cryptoApi";
 import Pagination from "../modules/Pagination";
 import Search from "../modules/Search";
+import Chart from "../modules/Chart";
 function HomePages() {
   const [coins, setCoins] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [page, setPage] = useState(1);
   const [currency, setCurrency] = useState("usd");
+  const [chart, setChart] = useState(null);
+
   useEffect(() => {
     setIsLoading(true);
     const getData = async () => {
@@ -25,8 +28,9 @@ function HomePages() {
   return (
     <div>
       <Search currency={currency} setCurrency={setCurrency} />
-      <TableCoin coins={coins} isLoading={isLoading} />
+      <TableCoin coins={coins} isLoading={isLoading} setChart={setChart} />
       <Pagination page={page} setPage={setPage} />
+      {!!chart && <Chart chart={chart} setChart={setChart} />}
     </div>
   );
 }
